0225adca302fe37037030a1e742a211c9a049fd4
Thorsten Dischereit <mr.t@MacBook-Pro.local>
2026-06-08 23:09:30 +0200

    Matrix Theme installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import Welcome from "@/components/Welcome";
 import ShimmerQuote from "@/components/ShimmerQuote";
 import BootcampFooter from "@/components/BootcampFooter";
+import MatrixRain from "@/components/MatrixRain";
 
 export default function Home() {
   const motto = process.env.NEXT_PUBLIC_MOTTO;
 
   return (
     <main className="min-h-screen flex flex-col">
+      <MatrixRain />
       <div className="flex-1 flex items-center justify-center px-6">
         {motto ? <ShimmerQuote text={motto} /> : <Welcome />}
       </div>
-
       <BootcampFooter />
     </main>
   );
